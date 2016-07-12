e589dc6a0099de835415f3bc468ff478243c527d
Annie Hoogendoorn <anniehoogendoorn@gmail.com>
2016-07-12 11:03:24 -0700

    Add processData function

--- process.js
+++ process.js
@@ -1,35 +1,42 @@
 var result = {};
-
-// $(document).ready(function () {
-
-  // function processMortality() {
-
-    var mortalityData, countryData, mortalityData_key, mortalityData_value;
-
-    $.when(
-      $.getJSON("mortality.json", function(data) {
-        mortalityData = data;
-      }),
-      $.getJSON("country-codes.json", function(data) {
-        countryData = data;
-      })
-    ).done(function() {
-      // var result = {};
-      for (var i=0; i<mortalityData.length; i++) {
-        mortalityData_key = mortalityData[i].key;
-        mortalityData_value = mortalityData[i].value;
-
-        $.each(countryData, function(key, value) {
-          if (mortalityData_key === value ) {
-            result[key] = mortalityData_value;
-          }
-        });
-      }
-      // return result;
-      // console.log(result);
-    });
-  // }
-
-// });// document ready
 var gdpData = result;
+// debugger;
+
+function processData(jsonData) {
+
+  var mortalityData, countryData;
+  // var result = {};
+
+  //Get JSON files and save gapminder object array to mortalityData and country code object to countrydata
+  $.when(
+    $.getJSON(jsonData, function(data) {
+      mortalityData = data;
+    }),
+    $.getJSON("mti-country-codes.json", function(data) {
+      countryData = data;
+    })
+    //Loop through gapminder json data and save values of country and stat to variables
+  ).done(function() {
+    for (var i=0; i<mortalityData.length; i++) {
+      var mortalityData_country = mortalityData[i].country;
+      var mortalityData_stat = mortalityData[i].stat;
+      // Loop through countryData object and if country matches gapminder data country, save key (country code) to equal stat to result
+      $.each(countryData, function(key, value) {
+        if (mortalityData_country === value ) {
+          result[key] = mortalityData_stat;
+        }
+      });
+    }
+    // debugger;
+    // console.log(result);
+    return result;
+  });
+}
+debugger;
+processData("mortality.json");
+
+
 console.log(result);
+// processData("physicians.json");
+// var physicianData = result;
+// console.log(physicianData);
