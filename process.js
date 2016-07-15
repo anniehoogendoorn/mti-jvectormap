484db2af09fd8463ed74b2176cb0ecd839b9a10d
Annie Hoogendoorn <anniehoogendoorn@gmail.com>
2016-07-15 10:28:50 -0700

    Add image to markers

--- process.js
+++ process.js
@@ -1,42 +1,43 @@
-var result = {};
-var gdpData = result;
-// debugger;
+var mortalityResult = {};
+var physicianResult ={};
 
-function processData(jsonData) {
+function processData(jsonData, result) {
 
-  var mortalityData, countryData;
-  // var result = {};
+  var gapminderData, countryData;
 
-  //Get JSON files and save gapminder object array to mortalityData and country code object to countrydata
+  //Get JSON files and save gapminder object array to gapminderData and country code object to countrydata
   $.when(
     $.getJSON(jsonData, function(data) {
-      mortalityData = data;
+      gapminderData = data;
     }),
     $.getJSON("mti-country-codes.json", function(data) {
       countryData = data;
     })
     //Loop through gapminder json data and save values of country and stat to variables
   ).done(function() {
-    for (var i=0; i<mortalityData.length; i++) {
-      var mortalityData_country = mortalityData[i].country;
-      var mortalityData_stat = mortalityData[i].stat;
+    for (var i=0; i<gapminderData.length; i++) {
+      var gapminderData_country = gapminderData[i].country;
+      var gapminderData_stat = gapminderData[i].stat;
       // Loop through countryData object and if country matches gapminder data country, save key (country code) to equal stat to result
       $.each(countryData, function(key, value) {
-        if (mortalityData_country === value ) {
-          result[key] = mortalityData_stat;
+        if (gapminderData_country === value ) {
+          result[key] = gapminderData_stat;
         }
       });
     }
-    // debugger;
-    // console.log(result);
     return result;
   });
 }
-debugger;
-processData("mortality.json");
+processData("mortality.json", mortalityResult);
+console.log(mortalityResult);
+processData("physician.json", physicianResult);
 
-
-console.log(result);
-// processData("physicians.json");
-// var physicianData = result;
-// console.log(physicianData);
+// $('.info-window').on('mouseover', function() {
+//   $(this).parent().addClass('is-hover');
+// }).on('mouseout', function() {
+//   $(this).parent().removeClass('is-hover');
+// });
+// debugger;
+$('circle.jvectormap-marker.jvectormap-element').click(function() {
+  console.log('this works');
+});
