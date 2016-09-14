eb50c48c9c11e660dec665db030d5405e1edf5d5
Annie Hoogendoorn <anniehoogendoorn@gmail.com>
2016-09-14 15:07:21 -0700

    Update map id's in script

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,19 +2,19 @@
 $('.map-menu button').click(function() {
   $('button').not(this).removeClass("selected");
   $(this).addClass('selected');
-    if ($(this).attr('id') == 'child-btn') {
+    if ($(this).attr('id') == 'btn-2') {
       $('.map-container').children().hide();
       $('#map-2').fadeIn(800);
     }
-    else if ($(this).attr('id') == "marriage_btn") {
+    else if ($(this).attr('id') == "btn-4") {
       $('.map-container').children().hide();
-      $('#marriage').fadeIn(800);
+      $('#map-4').fadeIn(800);
     }
 
-  toggle all sibling buttons
-  toggleClass this button
-
-  fadeToggle their corresponding map
+  // toggle all sibling buttons
+  // toggleClass this button
+  //
+  // fadeToggle their corresponding map
 });
 
 
