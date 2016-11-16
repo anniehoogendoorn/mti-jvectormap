e82ffed5acf3ff3d0710bac45d2db85f18ea90eb
Annie Hoogendoorn <anniehoogendoorn@gmail.com>
2016-11-16 11:32:27 -0800

    Add map one

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,7 +2,7 @@
 
 // Add click to button 1 on load
 $(document).ready(function() {
-  $('.map-menu button#btn2').trigger('click');
+  $('.map-menu button#btn1').trigger('click');
 });
 
 // $('.maps-container:not(.pulse) ').click(function() {
@@ -23,7 +23,7 @@ $('.map-menu button').click(function() {
   else if ($(this).attr('id') === 'btn2') {
     $('.maps-container > div:not(#map2-container)').hide();
     $('#map2-container').fadeIn(800);
-
+    $('#map2').vectorMap('get','mapObject').updateSize();
 
   }
   else if ($(this).attr('id') == "btn3"){
@@ -51,6 +51,99 @@ $('.map-menu button').click(function() {
 // JVECTORMAPS API
 
 // Map 1
+$(function(){
+  var scope = {};
+  scope.bgColor = 'transparant';
+  scope.scaleColors = ['#cb333b','#fffdfd'];
+  scope.fillColor = '#e7e7e9';
+  scope.strokeColor = '#fff';
+  scope.strokeWidth = '0.4';
+  scope.markerColor = '#21ACDE';
+  scope.markerStrokeColor = '#1a8ab2';
+
+
+  $('#map1').vectorMap({
+    map: 'world_mill_en',
+    backgroundColor: scope.bgColor,
+    zoomOnScroll: false,
+    regionStyle: {
+      initial: {
+        fill: scope.fillColor,
+        stroke: scope.strokeColor,
+        "stroke-width": scope.strokeWidth
+      }
+    },
+    markers: markers,
+    markerStyle: {
+      initial: {
+        fill: scope.markerColor,
+        stroke: scope.markerStrokeColor
+        // image: 'marker4.png'
+      },
+      hover: {
+        "fill-opacity": 1,
+        stroke: scope.markerStrokeColor,
+        cursor: 'pointer'
+      }
+
+    },
+    series: {
+      regions: [{
+        values: birthsData,
+        scale: scope.scaleColors,
+        normalizeFunction: 'polynomial',
+        legend: {
+          horizontal: true,
+          title: 'Percentage of Total'
+        }
+      }]
+      // markers: [{
+      //   attribute: fill,
+      //
+      // }]
+    },
+    onRegionTipShow: function(e, label, index){
+      label.html(label.html()+'<br> (Rate: '+birthsData[index]+')');
+    },
+    onMarkerTipShow: function(event, tip, index){
+      tip.html('<div class="info-window"><h3>'+tip.html()+'</h3>'+'<p>'+markers[index].content+'</p>'+'<a href="'+markers[index].link+'" target="_blank">Where we work <span>&#62;&#62;</span></a></div>');
+      event.preventDefault();
+    },
+    onMarkerOver: function(event, tip, code){
+      // console.log('You have triggered mouseover');
+
+
+    },
+    onMarkerClick: function(event, tip, code ) {
+      console.log('You have triggered click');
+      var map = $('#map1').vectorMap('get', 'mapObject');
+      var customTip = $('#customTip');
+
+
+
+      customTip.css({
+                left: left,
+                top: top
+              })
+
+      customTip.html(map.tip.html());
+      customTip.prepend("<button>&#215;</button>");
+      customTip.children("button").click(function(){
+                 customTip.hide();
+               })
+
+         // customTip.html('<h3>'+tip.html()+'</h3>'+'<p>'+markers[index].content+'</p>'+'<a href="'+markers[index].link+'">Where we work >></a>')
+      customTip.show();
+    }
+  });// end vectorMap
+
+  var left,top;
+  $('#map1').vectorMap('get', 'mapObject').container.mousemove(function(e){
+    left = e.pageX - 320;
+    top = e.pageY - 220;
+  });
+
+});// end function
 
 
 // Map 2
@@ -81,7 +174,6 @@ $(function(){
       initial: {
         fill: scope.markerColor,
         stroke: scope.markerStrokeColor
-        // image: 'marker4.png'
       },
       hover: {
         "fill-opacity": 1,
@@ -94,15 +186,12 @@ $(function(){
       regions: [{
         values: mortalityData,
         scale: scope.scaleColors,
-        normalizeFunction: 'polynomial',
+        // normalizeFunction: 'polynomial',
         legend: {
-          vertical: true
+          horizontal: true,
+          title: 'Per 1000 live births'
         }
       }]
-      // markers: [{
-      //   attribute: fill,
-      //
-      // }]
     },
     onRegionTipShow: function(e, label, index){
       label.html(label.html()+'<br> (Rate - '+mortalityData[index]+')');
